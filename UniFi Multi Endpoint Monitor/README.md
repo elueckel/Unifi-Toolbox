a78d58747abf633f8361c185446e16b3d5b53470
elueckel <30531515+elueckel@users.noreply.github.com>
2023-12-01 23:00:48 +0100

    Multi Endpoint Added

---
+++ UniFi Multi Endpoint Monitor/README.md
@@ -0,0 +1,91 @@
+# UniFi Multi Endpoint Monitor
+Dieses Modul ermöglicht es viele Endgeräte im Netz zu überwachen und diverse Daten in Symcon darzustellen.
+
+## Inhaltsverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Software-Installation](#3-software-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Versionsinformation](#5-versionsinformation)
+
+## 1. Funktionsumfang
+
+* Unterstützung für UniFi CloudKey 1 (UC-CK)
+* Unterstützung für UniFi CloudKey 2 (UCK-G2) und DreamMachine (UDM)
+* Anlegen von zu überwachenden Geräten mit Name und MAC Adresse 
+* Erstellt pro Gerät eine Variable welche z.B. für die Automation oder Überwachung genutzt werden kann (Boolean)
+* Abfragen der Controller erfolgt zeitgesteuert alle xx Sekunden
+
+## 2. Voraussetzungen
+
+- IP-Symcon ab Version 5.5
+- lokaler User (nicht Owner mit Mailadresse!)
+
+## 3. Software-Installation
+
+* Über den Module Store das 'UniFi Presence Manager'-Modul installieren.
+* Alternativ über das Module Control folgende URL hinzufügen
+
+## 4. Einrichten der Instanzen in IP-Symcon
+
+ Unter 'Instanz hinzufügen' kann das 'UniFi Presence Manager'-Modul mithilfe des Schnellfilters gefunden werden.  
+	- Weitere Informationen zum Hinzufügen von Instanzen in der [Dokumentation der Instanzen](https://www.symcon.de/service/dokumentation/konzepte/instanzen/#Instanz_hinzufügen)
+
+__Konfigurationsseite__:
+
+**Art des Controllers**
+
+Da sich die APIs von CloudKey 1 und CloudKey2/DreamMachine unterscheiden, kann hier der Controller gewählt werden
+
+**Benutzername & Kennwort**
+
+Account mit dem sich das Modul mit dem Controller verbindet
+
+**Site**
+
+Site-Name der im Controller hinterlegt ist (Standard: "default").
+
+ACHTUNG: Nicht mit Site-Description verwechseln, welche bspw. in der Controller GUI geändert werden kann!
+
+Der Site-Name kann überprüft werden mit einem Klick auf "check Site Name".
+
+**IP-Adresse und Port**
+
+Bei der DreamMachine ist der Port 443, bei einem Controller im Standard 8443. IP-Adresse des CloudKeys oder der DreamMachine.
+
+**Aktualisierungsfrequenz**
+
+Da der Controller aktiv abfragt werden muss, kann man hier eine Frequenz hinterlegen wie oft dies geschehen soll. 
+
+**Tabelle für Endgeräte**
+In der Tabelle die Geräte die überwacht werden sollen eintragen. Hierfür muss ein Name vergeben werden (mindestens 3 Buchstaben), die MAC Adresse und die Art der Verbindung.
+
+**MAC Adresse**
+
+Die MAC Adresse des zu überwachenden Geräts. Diese am besten aus dem Controller auslesen.
+
+**Art der Verbindung**
+
+Basierend auf der Art der Verbindung stehen unterschiedliche Informationen zur Verfügung. Die meisten werden von Geräten im WLAN generiert. 
+
+**Netzwerkdaten**
+
+Netzwerkdaten umfassen logische Daten wie IP-Adresse oder Hostname. 
+
+**Verbindungsdaten**
+
+Hier werden verfügbare physische Daten bereitgestellt. Hierzu zählen bei WLAN-Geräten Informationen zu Verbindungsqualität, Zuletzt gesehen, Uptime, Kanal, Art der Technologie, SSID, Dämpfung, Signalstärke. 
+
+**Übertragungsdaten**
+
+Bei WLAN-Geräten werden hier Informationen zu übertragenen Daten und Paketen geliefert.
+
+**Debugging**
+
+Das Modul gibt diverse Informationen im Debug Bereich aus. 
+
+## 5. Versionsinformation
+
+Version 1.5 - 02-12-2023
+* Neu - Modul verfügbar
---
+++ UniFi Multi Endpoint Monitor/module.php
@@ -0,0 +1,322 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__.'/../libs/myFunctions.php';  // globale Funktionen
+include_once __DIR__.'/../libs/timetest.php';
+
+// Modul Prefix
+if (!defined('MODUL_PREFIX'))
+{
+	define("MODUL_PREFIX", "UMEM");
+}
+
+class UnifiMultiEndpointMonitor extends IPSModule
+{
+	use myFunctions;
+    use TestTime;
+
+	public function Create()
+	{
+		//Never delete this line!
+		parent::Create();
+
+		$this->RegisterPropertyInteger("ControllerType", 0);
+		$this->RegisterPropertyString("ServerAddress", "192.168.1.1");
+		$this->RegisterPropertyInteger("ServerPort", "443");
+		$this->RegisterPropertyString("Site", "default");
+		$this->RegisterPropertyString("UserName", "");
+		$this->RegisterPropertyString("Password", "");
+		$this->RegisterPropertyInteger("Timer", "0");
+
+		$this->RegisterPropertyString("DeviceMac", "");
+
+		$this->RegisterPropertyString("Devices", "");
+
+		//$this->RegisterPropertyInteger("ConnectionType", 0);
+
+		$this->RegisterPropertyBoolean("DataPointNetwork", 0);
+		$this->RegisterPropertyBoolean("DataPointConnection", 0);
+		$this->RegisterPropertyBoolean("DataPointTransfer", 0);
+
+		$this->RegisterTimer("UniFi Multi Endpoint Monitor", 0, MODUL_PREFIX."_EndpointMonitor(\$_IPS['TARGET']);");
+	}
+
+	public function Destroy()
+	{
+		//Never delete this line!
+		parent::Destroy();
+	}
+
+	public function ApplyChanges()
+	{
+		//Never delete this line!
+		parent::ApplyChanges();
+		$vpos = 10;
+
+		//$this->RegisterVariableBoolean('Connected', $this->Translate('Connected'));
+
+		$DevicesList = $this->ReadPropertyString("Devices");
+		$DevicesJSON = json_decode($DevicesList, true);
+
+		if (isset($DevicesJSON))
+		{
+			foreach ($DevicesJSON as $Device)
+			{
+				$DeviceName = $Device["varDeviceName"];
+				$DeviceMac = $this->removeInvalidChars($Device["varDeviceMAC"], true);
+				$ConnectionType = $Device["varDeviceConnectionType"];
+
+				$vpos = 10;
+
+				$this->RegisterVariableBoolean($DeviceMac."_Connected", $DeviceName.$this->Translate(' Connected'));
+
+				//Network Data
+				$this->MaintainVariable($DeviceMac."IPAddress", $DeviceName.$this->Translate(" IP Address"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointNetwork") == 1);
+				$this->MaintainVariable($DeviceMac."Hostname", $DeviceName.$this->Translate(" Hostname"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointNetwork") == 1);
+
+				//Connection Data General
+				$vpos = 100;
+				$this->MaintainVariable($DeviceMac."Satisfaction", $DeviceName.$this->Translate(" Satisfaction"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1);
+				$this->MaintainVariable($DeviceMac."LastSeen", $DeviceName.$this->Translate(" Last Seen"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1);
+				$this->MaintainVariable($DeviceMac."Uptime", $DeviceName.$this->Translate(" Uptime in hours"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1);
+				//Connection Data Wired
+				$vpos = 300;
+				//$this->MaintainVariable("SwitchPort", $this->Translate("Switch Port"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 AND $this->ReadPropertyInteger("ConnectionType") == 1);
+				//$this->MaintainVariable("SwitchMAC", $this->Translate("Switch MAC"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 AND $this->ReadPropertyInteger("ConnectionType") == 1);
+				//Connection Data Wireless
+				$vpos = 500;
+				$this->MaintainVariable($DeviceMac."Accesspoint", $DeviceName.$this->Translate(" Accesspoint"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."Channel", $DeviceName.$this->Translate(" Channel"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."Radio", $DeviceName.$this->Translate(" Radio"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."ESSID", $DeviceName.$this->Translate(" ESS ID"), vtString, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."RSSI", $DeviceName.$this->Translate(" RSSI"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."Noise", $DeviceName.$this->Translate(" Noise"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."SignalStrength", $DeviceName.$this->Translate(" Signal Strength"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0);
+
+				//Transfer Data
+				$vpos = 600;
+				$this->MaintainVariable($DeviceMac."TXBytes", $DeviceName.$this->Translate(" TX Megabytes"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointTransfer") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."RXBytes", $DeviceName.$this->Translate(" RX Megabytes"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointTransfer") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."TXPackets", $DeviceName.$this->Translate(" TX Packets"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointTransfer") == 1 && $ConnectionType == 0);
+				$this->MaintainVariable($DeviceMac."RXPackets", $DeviceName.$this->Translate(" RX Packets"), vtInteger, "", $vpos++, $this->ReadPropertyBoolean("DataPointTransfer") == 1 && $ConnectionType == 0);
+			}
+		}
+
+		$TimerMS = $this->ReadPropertyInteger("Timer") * 1000;
+		$this->SetTimerInterval("UniFi Multi Endpoint Monitor", $TimerMS);
+
+		if (0 == $TimerMS)
+		{
+			// instance inactive
+			$this->SetStatus(104);
+		}
+		else
+		{
+			// instance active
+			$this->SetStatus(102);
+		}
+	}
+
+
+	public function AuthenticateAndGetData(string $UnifiAPI = "")
+	{
+		$ControllerType = $this->ReadPropertyInteger("ControllerType");
+		$ServerAddress = $this->ReadPropertyString("ServerAddress");
+		$ServerPort = $this->ReadPropertyInteger("ServerPort");
+		$Username = $this->ReadPropertyString("UserName");
+		$Password = $this->ReadPropertyString("Password");
+		$Site = $this->ReadPropertyString("Site");
+		$DeviceMac = strtolower($this->ReadPropertyString("DeviceMac"));
+
+		//Change the Unifi API to be called here
+		if ("" == $UnifiAPI)
+		{
+			$Site = $this->ReadPropertyString("Site");
+			//$UnifiAPI = "api/s/".$Site."/stat/sta"."/".$DeviceMac;
+			$UnifiAPI = "api/s/".$Site."/stat/sta";
+		}
+
+		//Generic Section providing for Authenthication against a DreamMachine or Classic CloudKey
+		$Cookie = $this->getCookie($Username, $Password, $ServerAddress, $ServerPort, $ControllerType);
+
+		// Section below will collect and return the RawData
+		if (!isset($Cookie) || false == $Cookie)
+		{
+			return false;
+		}
+		else
+		{
+			$RawData = $this->getRawData($Cookie, $ServerAddress, $ServerPort, $UnifiAPI, $ControllerType);
+			return $RawData;
+		}
+	}
+
+	public function EndpointMonitor()
+	{
+		$ControllerType = $this->ReadPropertyInteger("ControllerType");
+		$ServerAddress = $this->ReadPropertyString("ServerAddress");
+		$ServerPort = $this->ReadPropertyInteger("ServerPort");
+		$Username = $this->ReadPropertyString("UserName");
+		$Password = $this->ReadPropertyString("Password");
+		$Site = $this->ReadPropertyString("Site");
+		$DeviceMac = strtolower($this->ReadPropertyString("DeviceMac"));
+
+		//$UnifiAPI = "api/s/".$Site."/stat/sta"."/".$DeviceMac;
+		$UnifiAPI = "api/s/".$Site."/stat/sta";
+
+		//Generic Section providing for Authenthication against a DreamMachine or Classic CloudKey
+		$Cookie = $this->getCookie($Username, $Password, $ServerAddress, $ServerPort, $ControllerType);
+
+		// Section below will collect and return the RawData
+		if (!isset($Cookie) || false == $Cookie)
+		{
+			return false;
+		}
+		else
+		{
+			$RawData = $this->getRawData($Cookie, $ServerAddress, $ServerPort, $UnifiAPI, $ControllerType);
+		}
+
+
+		if (false !== $RawData)
+		{
+			if ($RawData !== "")
+			{
+				$JSONData = json_decode($RawData, true);
+				$ActiveDevices = $JSONData;
+
+				//Load devices from config form
+				$DevicesList = $this->ReadPropertyString("Devices");
+				$DevicesJSON = json_decode($DevicesList, true);
+
+				foreach ($DevicesJSON as $Device)
+				{
+					//Build a clean array out of the devices mentioned in the config form with : or -
+					$DeviceMac = $this->removeInvalidChars($Device["varDeviceMAC"], true);
+					$DeviceName = $Device["varDeviceName"];
+					$ConnectionType = $Device["varDeviceConnectionType"];
+
+					//Itterate through all device and check if one of them matches the list in the config form.
+					foreach ($ActiveDevices["data"] as $Index => $DeviceFromController)
+					{
+						$DeviceFromControllerClean = $this->removeInvalidChars($DeviceFromController["mac"], true);
+
+						if ($DeviceMac == $DeviceFromControllerClean)
+						{
+							$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Device with Name was found: ").$DeviceName, 0);
+					
+							$ConnectionMethod = $DeviceFromController["is_wired"];
+
+							if ($ConnectionMethod == true && $this->ReadPropertyInteger("ConnectionType") == 0)
+							{
+								$ConnectionConfigError = true;
+								//$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Config error - device monitored is a wired device. Please select wired in the module configuration."), 0);
+							}
+							else
+							{
+								$ConnectionConfigError = false;
+							}
+
+							if ($this->ReadPropertyBoolean("DataPointNetwork") == 1)
+							{
+								$IPAddress = $DeviceFromController["ip"];
+								SetValue($this->GetIDForIdent($DeviceMac."IPAddress"), $IPAddress);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Network Data IP ").$IPAddress, 0);
+
+								if ("" != $DeviceFromController["hostname"])
+								{
+									$Hostname = $DeviceFromController["hostname"];
+								}
+								elseif ("" != $DeviceFromController["name"])
+								{
+									$Hostname = $DeviceFromController["name"];
+								}
+								else
+								{
+									$Hostname = "";
+								}
+								SetValue($this->GetIDForIdent($DeviceMac."Hostname"), $Hostname);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Network Data Hostname ").$Hostname, 0);
+							}
+							if ($this->ReadPropertyBoolean("DataPointConnection") == 1)
+							{
+								if (isset($DeviceFromController["satisfaction"])) {
+									$Satisfaction = isset($DeviceMac["satisfaction"]);
+									SetValue($this->GetIDForIdent($DeviceMac."Satisfaction"), $Satisfaction);
+								}
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Satisfaction ").$Satisfaction, 0);
+								$SLastSeen = $DeviceFromController["last_seen"];
+								SetValue($this->GetIDForIdent($DeviceMac."LastSeen"), gmdate("Y-m-d H:i:s", $SLastSeen));
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Last Seen ").gmdate("Y-m-d H:i:s", $SLastSeen), 0);
+								$Uptime = $DeviceFromController["uptime"];
+								SetValue($this->GetIDForIdent($DeviceMac."Uptime"), round($Uptime / 3600, 0));
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Uptime in hours ").round($Uptime / 3600, 0), 0);
+							}
+							if ($this->ReadPropertyBoolean("DataPointConnection") == 1 && $ConnectionType == 0 && $ConnectionConfigError == false)
+							{
+								$Accesspoint = $DeviceFromController["ap_mac"];
+								SetValue($this->GetIDForIdent($DeviceMac."Accesspoint"), $Accesspoint);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Accesspoint ").$Accesspoint, 0);
+								$Channel = $DeviceFromController["channel"];
+								SetValue($this->GetIDForIdent($DeviceMac."Channel"), $Channel);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Channel ").$Channel, 0);
+								$Radio = $DeviceFromController["radio"];
+								SetValue($this->GetIDForIdent($DeviceMac."Radio"), $Radio);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Radio ").$Radio, 0);
+								$ESSID = $DeviceFromController["essid"];
+								SetValue($this->GetIDForIdent($DeviceMac."ESSID"), $ESSID);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data ESSID ").$ESSID, 0);
+								$RSSI = $DeviceFromController["rssi"];
+								SetValue($this->GetIDForIdent($DeviceMac."RSSI"), $RSSI);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data RSSI ").$RSSI, 0);
+								$Noise = $DeviceFromController["noise"];
+								SetValue($this->GetIDForIdent($DeviceMac."Noise"), $Noise);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data Noise ").$Noise, 0);
+								$SignalStrength = $DeviceFromController["signal"];
+								SetValue($this->GetIDForIdent($DeviceMac."SignalStrength"), $SignalStrength);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Connection Data SignalStrength ").$SignalStrength, 0);
+							}
+							if ($this->ReadPropertyBoolean("DataPointTransfer") == 1 && $ConnectionType == 0 && $ConnectionConfigError == false)
+							{
+								$TXBytes = $DeviceFromController["tx_bytes"];
+								SetValue($this->GetIDForIdent($DeviceMac."TXBytes"), $TXBytes / 1000000);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Transfer Data TXBytes ").$TXBytes / 1000000, 0);
+								$RXBytes = $DeviceFromController["rx_bytes"];
+								SetValue($this->GetIDForIdent($DeviceMac."RXBytes"), $RXBytes / 1000000);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Transfer Data RXBytes ").$RXBytes / 1000000, 0);
+								$TXPackets = $DeviceFromController["tx_packets"];
+								SetValue($this->GetIDForIdent($DeviceMac."TXPackets"), $TXPackets);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Transfer Data TXPackets ").$TXPackets, 0);
+								$RXPackets = $DeviceFromController["rx_packets"];
+								SetValue($this->GetIDForIdent($DeviceMac."RXPackets"), $RXPackets);
+								$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("Transfer Data RXPackets ").$RXPackets, 0);
+							}
+
+						}
+						else
+						{
+							//$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("NOT found - Device: ").$DeviceName, 0);
+						}
+					}
+				}
+			}
+			else
+			{
+				$this->SendDebug($this->Translate("Endpoint Monitor"), $this->Translate("There does not seem to be any configuration - no data is available from the UniFi"), 0);
+			}
+		}
+	}	
+
+	public function checkSiteName()
+	{
+		$ControllerType = $this->ReadPropertyInteger("ControllerType");
+		$ServerAddress = $this->ReadPropertyString("ServerAddress");
+		$ServerPort = $this->ReadPropertyInteger("ServerPort");
+		$Username = $this->ReadPropertyString("UserName");
+		$Password = $this->ReadPropertyString("Password");
+		$Site = $this->ReadPropertyString("Site");
+
+		return $this->getSiteName($Site, $Username, $Password, $ServerAddress, $ServerPort, $ControllerType);
+	}
+}
